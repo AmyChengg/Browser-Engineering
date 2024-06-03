7261bf4a10891bf07fa5eec874b7aff67d0aa2e5
Amy <36868219+AmyChengg@users.noreply.github.com>
2024-06-02 23:48:43 -0700

    Update draw_commands-ch9.py

--- draw_commands-ch9.py
+++ draw_commands-ch9.py
@@ -96,9 +96,3 @@ class DrawLine:
         return "DrawLine({}, {}, {}, {}, color={}, thickness={})".format(
             self.rect.left, self.rect.top, self.rect.right, self.rect.bottom,
             self.color, self.thickness)
-
-# def paint_tree(layout_object, display_list):
-#     display_list.extend(layout_object.paint())
-
-#     for child in layout_object.children:
-#         paint_tree(child, display_list)
